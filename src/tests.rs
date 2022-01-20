29e09de8c9dcccd676b53245097a84cc96e04376
Adrien Morison <adrien.morison@gmail.com>
2022-01-20 00:09:52 +0000

    More grid tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,3 @@
-use ndarray::Array1;
 use crate::{Grid1D, GridError};
 
 #[test]
@@ -9,7 +8,7 @@ fn grid_one_cell() {
 
 #[test]
 fn grid_one_cell_with_gc() {
-    let grid = Grid1D::new(1, 2, &[0., 1., 2., 3., 4., 5.]).unwrap();
+    let grid = Grid1D::new(1, 2, &[0., 1., 2., 3., 4., 5., 6.]).unwrap();
     assert_eq!(grid.span(), 2.);
 }
 
@@ -20,8 +19,20 @@ fn grid_singular() {
 }
 
 #[test]
-fn grid_not_enough_positions() {
-    let grid = Grid1D::new(1, 0, &[0.]);
+fn grid_no_gc_lower() {
+    let grid = Grid1D::new(1, 0, &[0., 0.5, 1., 1.5]);
+    assert!(matches!(grid, Err(GridError::MissingPositions)));
+}
+
+#[test]
+fn grid_no_gc_upper() {
+    let grid = Grid1D::new(1, 1, &[-0.5, 0., 0.5, 1.]);
+    assert!(matches!(grid, Err(GridError::MissingPositions)));
+}
+
+#[test]
+fn grid_not_enough_points() {
+    let grid = Grid1D::new(2, 1, &[-0.5, 0., 0.5, 1., 1.5]);
     assert!(matches!(grid, Err(GridError::MissingPositions)));
 }
 
