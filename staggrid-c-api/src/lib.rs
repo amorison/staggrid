022554aa4779e922f66018ec208ef5b44c103f99
Adrien Morison <adrien.morison@gmail.com>
2022-01-29 22:46:16 +0000

    Add grid_c_at in C API and related tests

--- staggrid-c-api/src/lib.rs
+++ staggrid-c-api/src/lib.rs
@@ -1,6 +1,12 @@
 #![deny(unsafe_op_in_unsafe_fn)]
 
-use staggrid::Grid1D;
+use libc;
+use staggrid::{Grid1D, Position};
+
+#[no_mangle]
+pub static POSITION_WALLS: u8 = 0;
+#[no_mangle]
+pub static POSITION_CENTERS: u8 = 1;
 
 #[no_mangle]
 pub unsafe extern "C" fn grid_c_create(
@@ -35,4 +41,48 @@ pub unsafe extern "C" fn grid_c_span(grid: *mut Grid1D) -> f64 {
     let span = grid.span();
     Box::leak(grid);
     span
+}
+
+fn position_from_int(int: u8) -> Option<Position> {
+    match int {
+        0 => Some(Position::Walls),
+        1 => Some(Position::Centers),
+        _ => None,
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn grid_c_at(
+    grid: *mut Grid1D,
+    position: u8,
+    length: *mut usize,
+    ierr: *mut i32,
+    ) -> *mut f64
+{
+    let grid = unsafe { Box::from_raw(grid) };
+    unsafe { *length = 0 };
+    let position = match position_from_int(position) {
+        Some(p) => p,
+        None => {
+            unsafe { *ierr = 1 };
+            return std::ptr::null_mut()
+        },
+    };
+
+    let values = grid.at(position);
+
+    let ptr = unsafe {
+        libc::malloc(std::mem::size_of::<f64>() * values.len())
+    } as *mut f64;
+    if ptr.is_null() {
+        unsafe { *ierr = -1 };
+        return ptr
+    }
+    for (i, &val) in values.iter().enumerate() {
+        unsafe { *ptr.add(i) = val };
+    }
+    unsafe { *length = values.len() };
+    unsafe { *ierr = 0 };
+    Box::leak(grid);
+    ptr
 }
--- staggrid-c-api/tests/grid.c
+++ staggrid-c-api/tests/grid.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdint.h>
 #include<stdbool.h>
 #include "staggrid.h"
 
@@ -45,11 +46,52 @@ bool grid_span() {
     return false;
 }
 
+bool grid_at_walls() {
+    double xs[5] = {-0.5, 0., 0.5, 1.0, 1.5};
+    int ierr;
+    struct Grid1D* grid = grid_c_create(1, 1, xs, 5, &ierr);
+    if (!grid) return false;
+    uintptr_t length;
+    double* walls = grid_c_at(grid, POSITION_WALLS, &length, &ierr);
+    if (!walls || ierr != 0 || length != 2) return false;
+    bool check = walls[0] == 0. && walls[1] == 1.0;
+    free(walls);
+    return check;
+}
+
+bool grid_at_centers() {
+    double xs[5] = {-0.5, 0., 0.5, 1.0, 1.5};
+    int ierr;
+    struct Grid1D* grid = grid_c_create(1, 1, xs, 5, &ierr);
+    if (!grid) return false;
+    uintptr_t length;
+    double* centers = grid_c_at(grid, POSITION_CENTERS, &length, &ierr);
+    if (!centers || ierr != 0 || length != 3) return false;
+    bool check = centers[0] == -0.5 && centers[1] == 0.5 && centers[2] == 1.5;
+    free(centers);
+    return check;
+}
+
+bool grid_at_invalid_position() {
+    double xs[5] = {-0.5, 0., 0.5, 1.0, 1.5};
+    int ierr;
+    struct Grid1D* grid = grid_c_create(1, 1, xs, 5, &ierr);
+    if (!grid) return false;
+    uintptr_t length;
+    double* centers = grid_c_at(grid, 2, &length, &ierr);
+    if (!centers && ierr == 1 && length == 0) return true;
+    if (centers) free(centers);
+    return false;
+}
+
 int main(void) {
 
     check(&grid_create, "grid_create\0");
     check(&grid_invalid, "grid_invalid\0");
     check(&grid_span, "grid_span\0");
+    check(&grid_at_walls, "grid_at_walls\0");
+    check(&grid_at_centers, "grid_at_centers\0");
+    check(&grid_at_invalid_position, "grid_at_invalid_position\0");
 
     return !result;
 }
